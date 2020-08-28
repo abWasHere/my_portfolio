70ab0bce3c9538fddf252d02f948b36488b7f132
abWasHere <audrey.belson@gmail.com>
2020-08-28 15:00:02 +0200

    first commit

---
+++ js/projectsList.js
@@ -0,0 +1,29 @@
+export default projects = [
+	{
+		id: 3,
+		name: "SPM",
+		description:
+			"Web application created to connect active people who want to organize sport themed trips. Designed with Node.js, Express .js, hbs and MongoDB.",
+		url: "https://sport-pour-meufs.herokuapp.com ",
+		source_code: "https://github.com/abWasHere/SPM-app-client",
+		release_date: "08/2020",
+	},
+	{
+		id: 2,
+		name: "Sportrip",
+		description:
+			"Web application created to connect active people who want to organize sport themed trips. Designed with Node.js, Express .js, hbs and MongoDB.",
+		url: "https://sportrip.herokuapp.com",
+		source_code: "https://github.com/auderichon/sporttrip",
+		release_date: "07/2020",
+	},
+	{
+		id: 1,
+		name: "Survive the city",
+		description:
+			"Front-end canvas game designed with HTML, CSS and JS DOM manipulation.",
+		url: "https://abwashere.github.io/survive-the-city",
+		source_code: "https://github.com/abWasHere/survive-the-city",
+		release_date: "06/2020",
+	},
+];
