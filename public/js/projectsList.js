8b1432e4c4638db9495f63594f76d6fad35d530a
abWasHere <audrey.belson@gmail.com>
2020-10-25 16:37:06 +0100

    puts assets, img, js and styles folders in a public folder

---
+++ public/js/projectsList.js
@@ -0,0 +1,48 @@
+export const projectsList = [
+	{
+		id: 4,
+		name: "NBA Stats",
+		usage: "Web app",
+		description:
+			"This app is using a public API to fetch informations regarding NBA players. <br/> Designed with REACT, deployed with Firebase Hosting.",
+		url: "https://nba-stats-2020.web.app",
+		source_code: "https://github.com/abWasHere/nba-stats",
+		img_url: "./public/images/nba-stats-capture.PNG",
+		release_date: "10/2020",
+		personal: true,
+	},
+	{
+		id: 3,
+		name: "SPM",
+		usage: "Web app, winner of the Ironhack Hackshow",
+		description:
+			"Designed to help women find a sport club around them and to help clubs attract and recruit new female members. <br/> Designed with REACT.",
+		url: "https://sport-pour-meufs.herokuapp.com ",
+		source_code: "https://github.com/abWasHere/SPM-app-client",
+		img_url: "./public/images/spm-capture.PNG",
+		release_date: "08/2020",
+	},
+	{
+		id: 2,
+		name: "Sportrip",
+		usage: "Web app",
+		description:
+			"Created to connect active people who want to organize sport themed trips. <br/> Designed with Node.js, Express .js, hbs and MongoDB.",
+		url: "https://sportrip.herokuapp.com",
+		source_code: "https://github.com/auderichon/sporttrip",
+		img_url: "./public/images/sportrip-capture.PNG",
+		release_date: "07/2020",
+	},
+	{
+		id: 1,
+		name: "Survive the city",
+		usage: "Game",
+		description:
+			"Front-end game designed with HTML, CSS and JS DOM manipulation.",
+		url: "https://abwashere.github.io/survive-the-city",
+		source_code: "https://github.com/abWasHere/survive-the-city",
+		img_url: "./public/images/survive-the-city-capture.PNG",
+		release_date: "06/2020",
+		personal: true,
+	},
+];
