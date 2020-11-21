b6832e6093ca8329c1e0261e804acdd06dd6284f
abWasHere <audrey.belson@gmail.com>
2020-11-21 23:05:35 +0100

    Major UI changes
    Modify project images, content, display
    Change backgrounds and main colors
    Modify skills content and display, add color
    Change change contact option

--- index.js
+++ index.js
@@ -1,67 +1,44 @@
 import { projectsList } from "./public/js/projectsList.js";
 
 /* GRID PROJECTS DISPLAY ----------------------------- */
+
 projectsList.map((project) => {
 	$("#cards-container").append(
-		`<div class="col">
-			<div class="card id${project.id} shadow p-3 bg-white rounded border-0" 
- >
-			<a href=${project.url} target="_blank"><img src=${
-			project.img_url
-		} class="card-img-top" alt=${project.name} screenshot"></a>
-				<div class="card-body">
-					<div class="card-titles">
-						<h5 class="card-title">${project.name}</h5>
-						<h6 class="card-subtitle mb-2 text-secondary ">${project.usage} - ${
-			project.release_date
-		}</h6>
-					</div>
-					<p class="card-text">${project.description}</p>
+		`
+			<div class="col id${project.id} ">
+
+				<img src=${project.img_url} class="project-img" alt=${project.name} screenshot">
+
+				<div class="project-content">
 					
-					<div class="card-bottom">
-						<p class="font-italic text-black-50">${
-							(project.personal &&
-								`<i class="far fa-user-circle"></i> Personal project`) ||
-							`<i class="fas fa-code-branch"></i> Collaboration`
-						} </p>
-						<a href=${project.url} class="card-link hvr-push" target="_blank">Demo</a>
-						<a href=${project.source_code} class="card-link hvr-push" target="_blank">
-							<i class="fas fa-code"></i>						Source
-						</a>
-					</div>
+					<h5 class="project-name">${project.name}</h5>
+
+					<div class="project-subtitle disp-flex">
+						<p class="project-date">${project.release_date}</p>
+					<p class="project-collaboration">${
+						(project.personal &&
+							`<i class="fas fa-user"></i> Personal project`) ||
+						`<i class="fas fa-users"></i> Collaboration`
+					}
+					</p>
+
 				</div>
+					
+				<p class="project-description">${project.description}</p>
+				<p class="project-tags">${project.tags}</p>
+						
+
+				<div class="disp-flex disp-centered project-links"> 
+					<a href=${project.url} class="hvr-push" target="_blank"> URL</a>
+					<a href=${
+						project.source_code
+					} class="hvr-push" target="_blank"><i class="fas fa-code"></i> Source</a>
+				</div>
+
 			</div>
-		</div>`
+		`
 	);
 });
-
-/* BTN DISCORD ----------------------------- */
-// ---- username popup
-// function toggleDiscordName() {
-// 	$("#discord-id").toggleClass("invisible");
-// }
-// $("#discord-link").on("click", toggleDiscordName);
-
-// ---- copy username to clipboard
-// var alertDisplay = false;
-// $("#discord-id").on("click", () => {
-// 	if (alertDisplay) {
-// 		$(".alert").fadeOut();
-// 		toggleDiscordName();
-// 		alertDisplay = false;
-// 	} else {
-// 		const el = document.createElement("textarea");
-// 		el.value = "Audwey#0769";
-// 		el.setAttribute("readonly", "");
-// 		el.style.position = "absolute";
-// 		el.style.left = "-9999px";
-// 		document.body.appendChild(el);
-// 		el.select();
-// 		document.execCommand("copy");
-// 		document.body.removeChild(el);
-// 		alertDisplay = true;
-// 	}
-// });
 
 /* BTN BACK-TO-TOP ----------------------------- */
 $(document).ready(function () {
